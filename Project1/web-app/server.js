c06ae0ac96f985df0fb978dfe0266a338c586133
tesco2 <tesco2@student.monash.edu>
2018-05-07 12:37:57 +1000

    Map stays at location

--- Project1/web-app/server.js
+++ Project1/web-app/server.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.render('mappage',{red: null,green:null,blue:null});
+  res.render('mappage',{red: null,green:null,blue:null, lat:-37.82701522389683, lng:144.96238708496094});
 })
 
 app.post('/',function (req,res){
@@ -19,7 +19,7 @@ app.post('/',function (req,res){
 		var pythondata = data.toString();
 		var RGB = pythondata.split("\n");
 		console.log(RGB[0]);
-		res.render('mappage',{red: RGB[0], green:RGB[1],blue:RGB[2]});
+		res.render('mappage',{red: RGB[0], green:RGB[1],blue:RGB[2], lat:Latitude, lng:Longitude});
 	});
 
 })
