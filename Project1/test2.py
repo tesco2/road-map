659f4d610e66ef6dd26591cde83a1ed3268d3e4d
tesco2 <tesco2@student.monash.edu>
2018-05-07 11:10:19 +1000

    Finished Website Functionality

---
+++ Project1/test2.py
@@ -0,0 +1,10 @@
+import requests
+from PIL import Image
+import sys
+
+
+lat = sys.argv[1]
+lng = sys.argv[2]
+
+print(lat,lng)
+sys.stdout.flush()
--- Project1/web-app/server.js
+++ Project1/web-app/server.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const spawn = require("child_process").spawn;
 
 app.set('view engine','ejs')
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.render('mappage');
+  res.render('mappage',{red: null,green:null,blue:null});
 })
 
 app.post('/',function (req,res){
-	res.render('mappage');
-	console.log("Hello");
+	var Latitude = req.body.lat;
+	var Longitude = req.body.lng;
+	var pythonProcess = spawn('python',["../test.py", Latitude, Longitude]);
+	pythonProcess.stdout.on('data',function (data){
+		var pythondata = data.toString();
+		var RGB = pythondata.split("\n");
+		console.log(RGB[0]);
+		res.render('mappage',{red: RGB[0], green:RGB[1],blue:RGB[2]});
+	});
+
 })
 
 app.listen(3000, function () {
